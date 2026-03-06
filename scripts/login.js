e8796b645632f182176ecfaf1a98ad24457e1421
shafikulnayem10 <inayem98@gmail.com>
2026-03-06 21:44:27 +0600

    implement login page and logic

---
+++ scripts/login.js
@@ -0,0 +1,29 @@
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", function () {
+
+  const username = document.getElementById("input-username").value;
+  const password = document.getElementById("input-password").value;
+
+ 
+  if (username === "" || password === "") {
+    alert("Please enter username and password");
+  }
+  else {
+    
+   
+    if (username === "admin" && password === "admin123") {
+     
+      window.location.assign("home.html");
+    }
+    else {
+      alert("Invalid username or password");
+    }
+
+  }
+
+});
+
+
+
+
